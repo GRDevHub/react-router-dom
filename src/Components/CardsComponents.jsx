ffc4c77440c37a5a9dc379defc9d515deca2a713
Gabriel Riveros <velo02.gr@gmail.com>
2024-09-22 13:53:59 -0400

    Add filtro de buscador con input

--- src/Components/CardsComponents.jsx
+++ src/Components/CardsComponents.jsx
@@ -1,14 +1,40 @@
 import PropTypes from 'prop-types';
 import Card from './Card';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 
 const CardsComponents = ({ personajes }) => {
+
+    const [searchParams, serSearchParams] = useSearchParams();
+
+    const handleChange = (e) => {
+        let filter = e.target.value;
+        serSearchParams({ filter: filter })
+    };
+
     return (
         <div className='mt-4 row row-cols-1 row-cols-md-3 g-4 border justify-content-center'>
-            {   personajes.map( item => (
-                    <Link key={ item.id } to={`/blog/${item.id}`}>
-                        <Card personaje={ item } />
-                    </Link>
+            <input 
+                className='form-control mb-2' 
+                type='text' 
+                placeholder='Personaje...'
+                onChange={ handleChange } 
+                value={ searchParams.get('filter') ? searchParams.get('filter') : '' } 
+            />
+            {   personajes
+                .filter( item => {
+                    let filter = searchParams.get('filter');
+                    let name = item.name.toLowerCase();
+
+                    if(!filter){
+                        return true;
+                    }else{
+                        return name.startsWith(filter.toLowerCase());
+                    }
+                })
+                .map( item => (
+                        <Link key={ item.id } to={`/blog/${item.id}`}>
+                            <Card personaje={ item } />    
+                        </Link>
                 ))
             }
         </div>
