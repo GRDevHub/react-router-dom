1a863c5af7e0d6223022512101a23443ffb37510
Gabriel Riveros <velo02.gr@gmail.com>
2024-09-22 16:14:04 -0400

    Update visualizacion depersonajes si no existe lo buscado renderizara personaje no encontrado

--- src/Components/CardsComponents.jsx
+++ src/Components/CardsComponents.jsx
@@ -4,12 +4,23 @@ import { Link, useSearchParams } from 'react-router-dom';
 
 const CardsComponents = ({ personajes }) => {
 
-    const [searchParams, serSearchParams] = useSearchParams();
+    const [searchParams, setSearchParams] = useSearchParams();
 
     const handleChange = (e) => {
         let filter = e.target.value;
-        serSearchParams({ filter: filter })
+        setSearchParams({ filter: filter });
     };
+
+    const perFilter = personajes.filter( item => {
+        let filter = searchParams.get('filter');
+        let name = item.name.toLowerCase();
+
+        if(!filter){
+            return true;
+        }else{
+            return name.startsWith(filter.toLowerCase());
+        }
+    });
 
     return (
         <div className='mt-4 row row-cols-1 row-cols-md-3 g-4 border justify-content-center'>
@@ -20,22 +31,13 @@ const CardsComponents = ({ personajes }) => {
                 onChange={ handleChange } 
                 value={ searchParams.get('filter') ? searchParams.get('filter') : '' } 
             />
-            {   personajes
-                .filter( item => {
-                    let filter = searchParams.get('filter');
-                    let name = item.name.toLowerCase();
-
-                    if(!filter){
-                        return true;
-                    }else{
-                        return name.startsWith(filter.toLowerCase());
-                    }
-                })
-                .map( item => (
-                        <Link key={ item.id } to={`/blog/${item.id}`}>
-                            <Card personaje={ item } />    
-                        </Link>
+            {   perFilter.length > 0
+                ? perFilter.map( item => (
+                    <Link key={ item.id } to={`/blog/${item.id}`}>
+                        <Card personaje={ item } />    
+                    </Link>
                 ))
+                : <h3>Personajes no encontrados</h3>
             }
         </div>
     );
